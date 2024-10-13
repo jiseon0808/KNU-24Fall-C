f82f7a9d10318186c66ba3b65fd4568e3bbe1b55
jiseon0808 <wltjsdl4061@naver.com>
2024-10-13 20:52:10 +0900

    Update day06-2.c

--- day06/day06-2.c
+++ day06/day06-2.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main(){
+    FILE* fp = NULL;
+    fp = fopen("data.txt", "wt");
+
+    fprintf(fp, "%d %d %d\n", 100, 200, 300);
+    fprintf(fp, "%d %d %d\n", 400, 500, 600);
+
+    fclose(fp);
+    return 0;
+}
