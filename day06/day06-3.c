01fbaed572fb961b02d01179e5e994cc09b925e2
jiseon0808 <wltjsdl4061@naver.com>
2024-10-13 20:52:29 +0900

    Update day06-3.c

--- day06/day06-3.c
+++ day06/day06-3.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+int main(){
+    FILE* fp = NULL;
+    fp = fopen("data.txt", "rt");
 
+    int n1, n2, n3;
+    while (fscanf(fp, "%d %d %d", &n1, &n2, &n3) != EOF) {
+        printf("n1 = %d, n1 = %d, n1 = %d\n", n1, n2, n3);
+    }
+
+    fclose(fp);
+    return 0;
+}
